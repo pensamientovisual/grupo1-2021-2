cf0fb7b13e96afa4693f36cb95403e8a08b21cca
paulaelicer <90005469+paulaelicer@users.noreply.github.com>
2021-10-11 22:18:46 -0300

    q

--- E2/extras.js
+++ E2/extras.js
@@ -1,26 +1,34 @@
 // BOTON A PAGINA TELEFONIA
 function boton1() {
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
     document.getElementById("paginatelefonia").style.visibility = "visible"
+    document.getElementById("paginaprincipal").style.visibility = "collapse"
+    document.getElementById("paginainternet").style.visibility = "collapse"
+    document.getElementById("paginacomercio").style.visibility = "collapse"
+    
 }
 // BOTON A PAGINA INTERNET
 function boton2() {
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
     document.getElementById("paginainternet").style.visibility = "visible"
+    document.getElementById("paginaprincipal").style.visibility = "collapse"
+    document.getElementById("paginatelefonia").style.visibility = "collapse"
+    document.getElementById("paginacomercio").style.visibility = "collapse"
 }
 
 // BOTON A PAGINA COMERCIO
 function boton3() {
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
     document.getElementById("paginacomercio").style.visibility = "visible"
+    document.getElementById("paginaprincipal").style.visibility = "collapse"
+    document.getElementById("paginatelefonia").style.visibility = "collapse"
+    document.getElementById("paginainternet").style.visibility = "collapse"
 }
 // BOTON A LA PAGINA PRINCIPAL
 function volver_atras() {
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
-
     document.getElementById("paginaprincipal").style.visibility = "visible"
+    document.getElementById("paginatelefonia").style.visibility = "collapse"
+    document.getElementById("paginainternet").style.visibility = "collapse"
+    document.getElementById("paginacomercio").style.visibility = "collapse"
+
+    
 }
 
 
