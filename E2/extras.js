d0969c64d834265d9a75dbfb3352e5cddd89677b
paulaelicer <90005469+paulaelicer@users.noreply.github.com>
2021-10-20 11:02:15 -0300

    botones arreglados

--- E2/extras.js
+++ E2/extras.js
@@ -1,29 +1,27 @@
 // BOTON A PAGINA TELEFONIA
 function boton1() {
-    document.getElementById("paginatelefonia").style.visibility = "visible"
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
-    document.getElementById("paginainternet").style.visibility = "collapse"
-    document.getElementById("paginacomercio").style.visibility = "collapse"
+    document.getElementById("paginainternet").style.display = "none"
+    document.getElementById("paginaprincipal").style.display = "none"
+    document.getElementById("paginacomercio").style.display = "none"
     
 }
 // BOTON A PAGINA INTERNET
 function boton2() {
-    document.getElementById("paginainternet").style.visibility = "visible"
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
-    document.getElementById("paginatelefonia").style.visibility = "collapse"
-    document.getElementById("paginacomercio").style.visibility = "collapse"
+    document.getElementById("paginatelefonia").style.display = "none"
+    document.getElementById("paginaprincipal").style.display = "none"
+    document.getElementById("paginacomercio").style.display = "none"
 }
 
 // BOTON A PAGINA COMERCIO
 function boton3() {
-    document.getElementById("paginacomercio").style.visibility = "visible"
-    document.getElementById("paginaprincipal").style.visibility = "collapse"
-    document.getElementById("paginatelefonia").style.visibility = "collapse"
-    document.getElementById("paginainternet").style.visibility = "collapse"
+    document.getElementById("paginatelefonia").style.display = "none"
+    document.getElementById("paginaprincipal").style.display = "none"
+    document.getElementById("paginainternet").style.display = "none"
 }
 // BOTON A LA PAGINA PRINCIPAL
 function volver_atras() {
-    document.getElementById("paginaprincipal").style.visibility = "visible"
-    document.getElementById("paginatelefonia").style.visibility = "collapse"
-    document.getElementById("paginainternet").style.visibility = "collapse"
-    document.getElementById("paginacomercio").style.visibility = "collapse"}
+    document.getElementById("paginaprincipal").style.display = "initial"
+    document.getElementById("paginatelefonia").style.display = "none"
+    document.getElementById("paginainternet").style.display = "none"
+    document.getElementById("paginacomercio").style.display = "none"
+}
