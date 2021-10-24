75f1fba92e2169cb2bf2380a4af8c7f986008138
anomvlito <84647703+anomvlito@users.noreply.github.com>
2021-10-23 22:40:27 -0300

    datos

---
+++ datos.js
@@ -0,0 +1,57 @@
+datos=[
+    {
+      "category": 2013,
+      "Total_cm": 9810982,
+      "amazon_billones_de_dolares": 74.45,
+      "llamadas": 299
+    },
+    {
+      "category": 2014,
+      "Total_cm": 10900147,
+      "amazon_billones_de_dolares": 88.99,
+      "llamadas": 365
+    },
+    {
+      "category": 2015,
+      "Total_cm": 11554069,
+      "amazon_billones_de_dolares": 104.83,
+      "llamadas": 374
+    },
+    {
+      "category": 2016,
+      "Total_cm": 13945254,
+      "amazon_billones_de_dolares": 135.99,
+      "llamadas": 407
+    },
+    {
+      "category": 2017,
+      "Total_cm": 16691468,
+      "amazon_billones_de_dolares": 177.87,
+      "llamadas": 489
+    },
+    {
+      "category": 2018,
+      "Total_cm": 18598053,
+      "amazon_billones_de_dolares": 232.89,
+      "llamadas": 545
+    },
+    {
+      "category": 2019,
+      "Total_cm": 19247205,
+      "amazon_billones_de_dolares": 280.52,
+      "llamadas": 613
+    },
+    {
+      "category": 2020,
+      "Total_cm": 20710225,
+      "amazon_billones_de_dolares": 386.06,
+      "llamadas": 503
+    },
+    {
+      "category": 2021,
+      "Total_cm": 21881359,
+      "amazon_billones_de_dolares": 221.6,
+      "llamadas": 325
+    }
+   ]
+
